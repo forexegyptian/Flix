47b70dd97d2333ecd74262588fdc3bc22d6e2a13
Dev <Dev@DevMail.com>
2017-11-18 11:42:42 +0200

    Hello React

---
+++ client/script.js
@@ -0,0 +1,6 @@
+ReactDOM.render(
+
+React.createElement("h2",null,"Hello React"),
+document.getElementById("root")
+
+);
--- server/index.js
+++ server/index.js
@@ -4,20 +4,30 @@
 
 const http = require('http');
 const server = http.createServer();
-server.on('request',(req,res)=>{
+const fs = require('fs');
+const express = require('express');
 
-	res.write("Hello Node \n");
+const app = express();
 
+app.use(express.static('client'));
+app.listen(8080);
+
+//server.on('request',(req,res)=>{
+//res.end(fs.readFileSync(__dirname+'/../client/index.html'));
+
+/*
+res.write("Hello Node \n");
 setTimeout(()=>{
 
 	res.write("Still on. ...\n");
 	res.end();
 },3000);
-
+*/
 	
-});
+//});
 
-server.listen(8080);
+//server.listen(8080);
+
 /*
 http.createServer((req,res)=>{
 
